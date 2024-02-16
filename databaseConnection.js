5cdab83246e8587958dadbf6faa04c1af82069ce
unknown <ye37seol@gmailcom>
2024-02-15 19:13:49 -0800

    fix error

--- databaseConnection.js
+++ databaseConnection.js
@@ -5,8 +5,8 @@ const is_render = process.env.IS_RENDER || false;
 const dbConfigRender = {
 	host: "sql.freedb.tech",
 	user: "freedb_2350_main.",
-	password: "ADv3rDufMJB62v&",
-	database: "freedb_freedb_COMP2350_A01342823",
+	password: "m?eg6bwvU8Qz8yz",
+	database: "freedb_COMP2350_A01342823",
 	multipleStatements: false
 };
 
